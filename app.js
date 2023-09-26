7439eec45af1743ea0c23c6fdbe48ba2c765654a
Mehmet Mert Altuntaş <mhmtmrtltnts@gmail.com>
2023-09-26 12:26:31 +0300

    ex

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
-console.log("mert")
-let language=["Aşk","Amore","Láska","Liefde",
-"Love", "Armastus","Amour", "Liebe","Αγάπη",
-"Aloha","प्यार","Любовь", "Sevgi", "愛","Amor",
-"사랑"]
+console.log("mert");
+let language = [
+  "Aşk",
+  "Amore",
+  "Láska",
+  "Liefde",
+  "Love",
+  "Armastus",
+  "Amour",
+  "Liebe",
+  "Αγάπη",
+  "Aloha",
+  "Cinta",
+  "Kärlek",
+  "प्यार",
+  "Любовь",
+  "ভালবাসা",
+  "Amor",
+  "사랑",
+];
 
-
-function word(){
-    return language[Math.floor(Math.random()*language.length)];
+function word() {
+  return language[Math.floor(Math.random() * language.length)];
 }
 
-function runTypewriter(){
-
-const text = document.querySelector("#ask");
-const strText = word();
-const splitText = strText.split("");
-text.textContent = "";
-for (let i=0; i < splitText.length; i++){
+function runTypewriter() {
+  const text = document.querySelector("#ask");
+  const strText = word();
+  const splitText = strText.split("");
+  text.textContent = "";
+  for (let i = 0; i < splitText.length; i++) {
     text.innerHTML += "<span>" + splitText[i] + "</span>";
-}
+  }
 
-let char=0;
-let timer=setInterval(onTick, 50);
+  let char = 0;
+  let timer = setInterval(onTick, 50);
 
-function onTick() {
+  function onTick() {
     const span = text.querySelectorAll("span")[char];
     span.classList.add("fade");
     char++;
-    if(char === splitText.length){
-        complete();
-        return;
+    if (char === splitText.length) {
+      complete();
+      return;
     }
-}
+  }
 
-function complete(){
+  function complete() {
     clearInterval(timer);
-    timer=null;
-}
+    timer = null;
+  }
 }
 //runTypewriter();
-setInterval(runTypewriter, 2000)
-
+setInterval(runTypewriter, 2000);
